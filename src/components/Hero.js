9c493a3ee481b93e621bd637d031c9b456aefb2e
Mrityunjay Raj <mr.raj.earth@gmail.com>
2026-04-13 07:22:09 +0530

    feat: add shipping solutions subline to hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,6 +10,7 @@ export default function Hero() {
         <div className="heroText">
           <p className="heroName">Hey, I&apos;m {siteConfig.name.split(" ")[0]}</p>
           <p className="heroTagline">{siteConfig.tagline}</p>
+          <p className="heroSubline">Shipping solutions that people actually use.</p>
         </div>
       </div>
       <a
